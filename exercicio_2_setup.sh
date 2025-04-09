c7f5446a8115d19fd4698d3a697f913d06384648
Henrique Rieger <henriquerieger2001@gmail.com>
2025-04-09 11:59:40 -0300

    [change] estrutura do script de setup do exercício 2

--- exercicio_2_setup.sh
+++ exercicio_2_setup.sh
@@ -3,6 +3,10 @@
 AVALIACAO=$HOME/avaliacao
 MUSCLE_ID=SRR6262201
 SKIN_ID=SRR7591066
+OUTPUT_DIR=$AVALIACAO/outputs
+
+# Create output directory if doesn't exist
+mkdir -p ${OUTPUT_DIR}
 
 # Download and uncompress clownfish reference transcriptome
 if ! [ -f clownfish.fasta ]; then
@@ -13,24 +17,41 @@ else
 fi
 
 # Execute FastQC analysis for original files
-if ! [ -f ${MUSCLE_ID}_fastqc.html ]; then
+if ! [ -f ${OUTPUT_DIR}/${MUSCLE_ID}_fastqc.html ]; then
   ln -s ${AVALIACAO}/${MUSCLE_ID}.fastq.gz ${MUSCLE_ID}.fastq.gz
   ln -s ${AVALIACAO}/${SKIN_ID}.fastq.gz ${SKIN_ID}.fastq.gz
-  fastqc -t 2 ${AVALIACAO}/${MUSCLE_ID}.fastq.gz ${AVALIACAO}/${SKIN_ID}.fastq.gz
+  fastqc \
+    -t 2 \
+    --outdir=${OUTPUT_DIR} \
+    ${AVALIACAO}/${MUSCLE_ID}.fastq.gz \
+    ${AVALIACAO}/${SKIN_ID}.fastq.gz
 else
   echo "Found FastQC result for first analysis. Skipping first FastQC..."
+fi
 
 # Trimm both muscle and skin FastQ files
 if ! [ -f ${MUSCLE_ID}.trimmed.fastq.gz ]; then
-  fastp -i ${MUSCLE_ID}.fastq.gz -o ${MUSCLE_ID}.trimmed.fastq.gz -h fastp_${MUSCLE_ID}_report.html -j fastp_${MUSCLE_ID}_report.json
-  fastp -i ${SKIN_ID}.fastq.gz -o ${SKIN_ID}.trimmed.fastq.gz -h fastp_${SKIN_ID}_report.html -j fastp_${SKIN_ID}_report.json
+  fastp \
+    -i ${MUSCLE_ID}.fastq.gz \
+    -o ${MUSCLE_ID}.trimmed.fastq.gz \
+    -h ${OUTPUT_DIR}/fastp_${MUSCLE_ID}_report.html \
+    -j ${OUTPUT_DIR}/fastp_${MUSCLE_ID}_report.json
+  fastp \
+    -i ${SKIN_ID}.fastq.gz \
+    -o ${SKIN_ID}.trimmed.fastq.gz \
+    -h ${OUTPUT_DIR}/fastp_${SKIN_ID}_report.html \
+    -j ${OUTPUT_DIR}/fastp_${SKIN_ID}_report.json
 else
   echo "Found trimmed muscle FastQ file. Skipping FastP..."
+fi
 
 # Re-run FastQC analysis for trimmed files
-if ! [ -f ${MUSCLE_ID}_trimmed_fastqc.html ]; then
-  fastqc -t 2 ${MUSCLE_ID}.trimmed.fastq.gz ${SKIN_ID}.trimmed.fastq.gz
+if ! [ -f ${OUTPUT_DIR}/${MUSCLE_ID}.trimmed_fastqc.html ]; then
+  fastqc \
+    -t 2 \
+    --outdir=${OUTPUT_DIR} \
+    ${MUSCLE_ID}.trimmed.fastq.gz \
+    ${SKIN_ID}.trimmed.fastq.gz
 else
   echo "Found FastQC result for second analysis. Skipping second FastQC..."
 fi
-
